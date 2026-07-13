410b55d7e2eae79dfab6b1c323b3a5f06d0feccc
Rindraniaina Rasolofondrasoa <rindraniainarasolofondrasoa17@gmail.com>
2026-07-13 09:19:50 +0300

    feat: service

---
+++ src/main/java/hei/school/async/service/ImageProcessingService.java
@@ -0,0 +1,36 @@
+package hei.school.async.service;
+
+import java.awt.color.ColorSpace;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageProcessingService {
+
+    @SneakyThrows
+    public File convertToBlackAndWhite(File inputFile, String format) {
+        BufferedImage sourceImage = ImageIO.read(inputFile);
+        if (sourceImage == null) {
+            throw new IOException("Could not read image file: " + inputFile.getName());
+        }
+
+        BufferedImage resultImage = new BufferedImage(
+            sourceImage.getWidth(), 
+            sourceImage.getHeight(), 
+            BufferedImage.TYPE_BYTE_GRAY
+        );
+
+        ColorConvertOp op = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
+        op.filter(sourceImage, resultImage);
+
+        File outputFile = File.createTempFile("bw-", "." + format);
+        ImageIO.write(resultImage, format, outputFile);
+        
+        return outputFile;
+    }
+}
